65a294bc4b79f702696b4a93a9a6a3f1994b8d05
Ben P <ben.passman@outlook.com>
2019-12-02 00:59:40 +1300

    Minor update to init MCP23017 output register. I/O Expander settings stored in EEPROM.

--- I2C Tests/main.c
+++ I2C Tests/main.c
@@ -32,15 +32,8 @@ uint8_t system_init()
 	/*	GPIO	*/
 	PORTC.DIRSET = PIN0_bm; // Set onboard LED as output
 	PORTC.PIN5CTRL = PORT_ISC_FALLING_gc; // Interrupt on button down
-	PORTB.DIRSET = PIN2_bm | PIN3_bm;
-	PORTA.DIRSET = PIN1_bm | PIN2_bm;
-	PORTA.DIRSET = PIN4_bm | PIN5_bm | PIN6_bm;
-
-	/*	TIMER/COUNTER A	*/
-	TCA0.SINGLE.PERL = 0xB7;
-	TCA0.SINGLE.PERH = 0x0C;
-	TCA0.SINGLE.INTCTRL |= TCA_SINGLE_OVF_bm;
-	TCA0.SINGLE.CTRLA |= TCA_SINGLE_CLKSEL_DIV1024_gc;
+	PORTB.DIRSET = PIN2_bm | PIN3_bm;  // UART
+	PORTA.DIRSET = PIN1_bm | PIN2_bm;	// TWI
 	
 	/*	I/O Lines	*/
 	PORTMUX.CTRLB = PORTMUX_TWI0_ALTERNATE_gc | PORTMUX_SPI0_ALTERNATE_gc | PORTMUX_USART0_DEFAULT_gc;
@@ -57,6 +50,10 @@ int main(void)
 {
 	system_init();
 	
+	// Should use (const unint8_t *) for EEPROM.
+	i2c_set_buffer((uint8_t *)0x1400, 2);
+	i2c_start(SLAVE1_ADDR, I2C_WRITE_bm);
+	
 	while (1)
     {
 		
@@ -67,21 +64,10 @@ ISR(PORTC_PORT_vect)
 {
 	uint8_t intflags = PORTC.INTFLAGS;
 	PORTC.INTFLAGS = intflags;
-	// Debug	
-	PORTC.OUTTGL = PIN0_bm; 
+	// Debug
+	PORTC.OUTTGL = PIN0_bm;
 	
 	i2c_set_buffer(bytes, 2);
 	i2c_start(SLAVE1_ADDR, I2C_WRITE_bm);
 	bytes[1] = bytes[1] >> 7 | bytes[1] << 1;
-	
-	// Clear GPIO and setup timer for debug
-	TCA0.SINGLE.CTRLESET |= TCA_SPLIT_CMD_RESTART_gc;
-	TCA0.SINGLE.CTRLA |= TCA_SINGLE_ENABLE_bm;
-	VPORTA.OUT &= ~(PIN4_bm | PIN5_bm | PIN6_bm);
-}
-
-ISR(TCA0_OVF_vect) {
-	TCA0.SINGLE.INTFLAGS |= TCA_SINGLE_OVF_bm;
-	TCA0.SINGLE.CTRLA &= ~TCA_SINGLE_ENABLE_bm;
-	VPORTA.OUT &= ~(PIN4_bm | PIN5_bm | PIN6_bm);
 }
